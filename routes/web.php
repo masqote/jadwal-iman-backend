95e10d4a8c8c78392a30ca4efa644ebcdac4dd9e
Medhy Pradana Putra <masqote@gmail.com>
2022-07-25 09:22:11 +0700

    Login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', 'App\Http\Controllers\AuthController@login')->name('login');
+Route::post('/login', 'App\Http\Controllers\AuthController@authenticate');
+
+Route::get('/dashboard', 'App\Http\Controllers\DashboardController@index')->middleware('auth');
